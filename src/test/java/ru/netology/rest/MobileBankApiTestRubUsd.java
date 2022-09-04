a6cb3863ee9153732199af60858c01cff69366b5
Артем <artem@MacBook-Pro-Artem.local>
2022-09-04 17:35:26 +0300

    add rub usd

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestRubUsd.java
@@ -0,0 +1,23 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
+public class MobileBankApiTestRubUsd {
+    @Test
+    void shouldReturnDemoAccountsRubUsd() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                // Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                // Проверки
+                .then()
+                .body("[0].currency", equalTo("RUB"));
+    }
+}
--- src/test/java/ru/netology/rest/MobileBankApiTestV1.java
+++ src/test/java/ru/netology/rest/MobileBankApiTestV1.java
@@ -19,19 +19,4 @@ class MobileBankApiTestV1 {
       .then()
           .statusCode(200);
     }
-
-    @Test
-    void shouldReturnDemoAccountsSchema() {
-        // Given - When - Then
-        // Предусловия
-        given()
-                .baseUri("http://localhost:9999/api/v1")
-                // Выполняемые действия
-                .when()
-                .get("/demo/accounts")
-                // Проверки
-                .then()
-                .statusCode(200)
-                .body(matchesJsonSchemaInClasspath("accounts.schema.json"));
-    }
 }
